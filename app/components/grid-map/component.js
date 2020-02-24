1ffae442d5ebd3cac04c45f00f61fa9b00b530ea
Omar Estrella <omarestrella@bitcreative.net>
2020-02-24 08:13:17 -0500

    Manipulate size in component

--- app/components/grid-map/component.js
+++ app/components/grid-map/component.js
@@ -1,13 +1,26 @@
 import Component from "@glimmer/component";
+import { tracked } from "@glimmer/tracking";
+import { action, set } from "@ember/object";
 
 import { generateGrid } from "../../map/map";
 
 export default class GridMap extends Component {
-  grid = generateGrid(15);
+  @tracked
+  size = 15;
 
   constructor() {
     super(...arguments);
 
     console.log("Grid", this.grid);
+  }
+
+  @action
+  setSize(event) {
+    let size = parseInt(event.target.value, 10);
+    set(this, "size", size);
+  }
+
+  get grid() {
+    return generateGrid(this.size);
   }
 }
